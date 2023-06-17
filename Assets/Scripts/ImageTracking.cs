61f69abbe41082d680a86b622a7be07505b38f9e
anticdope <pruettdillan@gmail.com>
2023-06-16 20:59:19 -0700

    Update ImageTracking.cs

--- Assets/Scripts/ImageTracking.cs
+++ Assets/Scripts/ImageTracking.cs
@@ -3,23 +3,34 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.XR;
 using UnityEngine.XR.ARFoundation;
+using UnityEngine.XR.ARSubsystems;
 
 public class ImageTracking : MonoBehaviour
 {
+    // No idea wtf a serialized field is but this is one
     [SerializeField]
     ARTrackedImageManager m_TrackedImageManager;
 
-    //When is this called?
+    // Declared array of prefabs
+    public GameObject[] ArPrefabs;
+
+    private readonly Dictionary<string, GameObject> _instantiatePrefabs = new Dictionary<string, GameObject>
+
+    // When is this called?
+    // According to docs MonoBehavior.OnEnable() is called when the game object is
+    // "enabled". Im not sure what enabled actually
+    // means and im also not sure when this particular gameobjectis enabled.
     void OnEnable() => m_TrackedImageManager.trackedImagesChanged += OnChanged;
 
-    //When is this called?
+    // When is this called?
+    // When the object is disabled... same conundrum as above
     void OnDisable() => m_TrackedImageManager.trackedImagesChanged -= OnChanged;
 
     void OnChanged(ARTrackedImagesChangedEventArgs eventArgs)
     {
         foreach (var newImage in eventArgs.added)
         {
-            Console.log(ARTrackedImage);
+            //What happens when a new image is added?
         }
 
         foreach (var updatedImage in eventArgs.updated)
